bf733efb1006276c98522c8b5e6267b98787b7c7
Joaquim <quimze1996.25@gmail.com>
2023-11-09 12:00:25 +0000

    Footer does not show on login and (future) sign-up page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import { usePathname } from 'next/navigation'
 
 
 function Layout({ children }: { children: React.JSX.Element }) {
+    const pathname = usePathname()
+
     return (
         <>
             <Navbar />
             <main>{children}</main>
-            <Footer />
+            {pathname !== "/login" && pathname !== "/signup" && <Footer />}
         </>
 
     )
